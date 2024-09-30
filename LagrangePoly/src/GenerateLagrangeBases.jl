a4ab46f32bf06af73631ffa5a3ad7ea1ae3f995e
Iffan Hannanu <iffan.hannanu@gmail.com>
2024-09-30 16:20:38 +0200

    convert to DynamicPolynomials

--- LagrangePoly/src/GenerateLagrangeBases.jl
+++ LagrangePoly/src/GenerateLagrangeBases.jl
@@ -1,44 +1,35 @@
 module GenerateLagrangeBases
 
-import Pkg; 
-using Combinatorics
-using Symbolics
-using LinearAlgebra
+# import Pkg;
+# Pkg.add("DynamicPolynomials")
+
+using DynamicPolynomials, LinearAlgebra, Combinatorics
 
 function multinomial_coefficient(powers::Vector{Int})
-    # return factorial(sum(powers)) / prod(factorial(p) for p in powers)
     return prod(factorial(p) for p in powers)
 end
 
-function generate_monomials_symbolic(n::Integer, m::Integer, x::Vector{Num})
-    # Generate all combinations of exponents with sum equal to m and n variables
+function generate_monomials_dynamic(n::Integer, m::Integer, x)
     monomial_powers = exponent_combinations(n, m)
     
-    # Generate monomials with normalization coefficients
     monomials = []
     for powers in reverse(monomial_powers)
         coefficient = multinomial_coefficient(powers)
-        monomial = prod(x[i]^powers[i] for i in 1:n)/coefficient
+        monomial = prod(x[i]^powers[i] for i in 1:n) / coefficient
         push!(monomials, monomial)
     end
     
     return monomials
 end
 
-# Helper function to generate exponent combinations that sum to m
 function exponent_combinations(n::Integer, m::Integer)
-    # Generate all combinations of n non-negative integers that sum up to exactly m
     comb = [tuple for tuple in multinomial(n, m)]
     return comb
 end
 
-# Helper function to compute multinomial (combinations of non-negative integers summing to m)
 function multinomial(n::Integer, m::Integer)
-    # We use the stars and bars approach to generate the partitions
-    # We choose n-1 dividers (bars) and m stars, and iterate over all possibilities
     nomials = []
     for stars in combinations(1:m+n-1, n-1)
-        
         result = [stars[1]-1; diff(stars) .- 1; m + n - 1 - stars[end]]
         push!(result, m - sum(result))
         push!(nomials, result)
@@ -47,54 +38,52 @@ function multinomial(n::Integer, m::Integer)
     return nomials
 end
 
-
 function compute_basis_matrix(data_points, basis, x)
-
     basis_matrix = []
     for i in axes(data_points, 1)
         inps = data_points[i, :]
         values = Dict(x[j] => inps[j] for j in eachindex(inps))
-        entry = Symbolics.value.(substitute.(basis, Ref(values)))
+        entry = [subs(b, values...) for b in basis]
         push!(basis_matrix, entry)
     end
 
     basis_matrix = hcat(basis_matrix...)
+    basis_matrix = Array{Float64}(basis_matrix)
 
+    # convert(Matrix(Float64), basis_matrix)
+    # Float64.(basis_matrix)
     return basis_matrix
 end
 
-function build_lagrange_basis_frobenius(data_points, basis, x)
-
+function build_lagrange_bases_frobenius(data_points, basis, x)
     M = compute_basis_matrix(data_points, basis, x)
+    println(M)
 
-    # Divide between linear and quadratic terms
     n = size(data_points, 2)
-    M_l = M[1:n+1, :]' # size = (p, n+1)
-    M_q = M[n+2:end, :]' # size = (p, b - (n+1))
+    M_l = M[1:n+1, :]' 
+    M_q = M[n+2:end, :]' 
     
     Vb_l = basis[1:n+1]
     Vb_q = basis[n+2:end]
 
-    # Build matrix F (eq 5.7)
+    println(M_q)
     A = M_q * M_q'
-    
     F = vcat(hcat(A, M_l), hcat(M_l', zeros(size(M_l, 2), size(M_l, 2))))
 
     Finv = pinv(real(F))
-    
-    # Construct (Eq 5.9) F x L = B
     B = vcat(M_q * Vb_q, Vb_l)
     S = Finv * B
     
-    polys = [simplify(S[i], expand=true) for i in 1:size(A, 1)]
+    polys = [S[i] for i in 1:size(A, 1)]
 
     return polys
 end
 
 function generate_lagrange_bases(n::Integer, m::Integer, d::Matrix{Float64})
-    x = Symbolics.variables(:x, 1:n)
-    linear_monomials = generate_monomials_symbolic(n, m-1, x)
-    quadratic_monomials = generate_monomials_symbolic(n, m, x)
+    @polyvar x[1:n]
+    println(x[1], x[2])
+    linear_monomials = generate_monomials_dynamic(n, m-1, x)
+    quadratic_monomials = generate_monomials_dynamic(n, m, x)
 
     monomials = []
     append!(monomials, [1])
