e3984967465bb48ca171fa533a4c9d57da205c37
Iffan Hannanu <iffan.hannanu@gmail.com>
2024-10-01 15:51:37 +0200

    fix computing poisedness using SOS

--- LagrangePoly/src/GenerateLagrangePoly.jl
+++ LagrangePoly/src/GenerateLagrangePoly.jl
@@ -6,8 +6,9 @@ module GenerateLagrangePoly
 
 include("GenerateLagrangeBases.jl")
 using DynamicPolynomials
-using Ipopt
 using JuMP
+using CSDP
+using SumOfSquares
 
 function generate_lagrange_poly(Y::Vector{Float64}, LBases)
     poly = 0
@@ -18,36 +19,33 @@ function generate_lagrange_poly(Y::Vector{Float64}, LBases)
 end
 
 function compute_poisedness(center::Vector{Float64}, radius::Float64, LBases)
-    p = Vector{Float64}()
+    solver = optimizer_with_attributes(CSDP.Optimizer, MOI.Silent() => true)
+
+    p = Vector{Float64}();
     for LBase in LBases
-        N = length(center)
-        model = Model(Ipopt.Optimizer)
-        @variable(model, x[1:N]) 
-        
-        the_sum = sum((x[j] - center[j])^2 for j in 1:N)
-        @NLconstraint(model, c1, sqrt(the_sum) <= radius)
-        
-         # Define a function to evaluate the polynomial
-        function evaluate_polynomial(LBase, x_vals::Vector{Float64}...)
-            values = Dict(Symbol("x$i") => x_vals[i] for i in eachindex(x_vals))
-            return subs(LBase, values)
-        end
+            model = SOSModel(solver)
 
-        # Register the function with JuMP
-        # JuMP.register(model, :evaluate_polynomial, N, (x...) -> evaluate_polynomial(LBase, x...); autodiff = true)
-        JuMP.register(model, :LBase, N, (x...) -> evaluate_polynomial(LBase, x...); autodiff = true)
+            @variable(model, α)
+            @objective(model, Min, α)
 
-        # Define the objective function using @NLobjective
-        @NLobjective(model, Max, evaluate_polynomial(LBase, x...))
+            x = DynamicPolynomials.variables(LBase)
+            the_sum = sum((x[j] - center[j])^2 for j in eachindex(center))
+            S = SumOfSquares.@set the_sum <= radius^2
+            @constraint(model, c2, LBase <= α, domain = S, maxdegree = 2)
 
-        optimize!(model)
+            optimize!(model)
+            
+            # FOR DEBUGGING
+            #println(solution_summary(model))
+            #θ = moment_matrix(model[:c2]);
+            #η = atomic_measure(θ, 1e-3);    
+            #x_opt = η.atoms[1].center
 
-        # Extract the result if needed
-        result = value.(x)
-        println(result)
+            SumOfSquares.unregister(model, :c2)
+            append!(p, solution_summary(model).objective_value)
     end
 
-    return
+    return p
 end
 
 
